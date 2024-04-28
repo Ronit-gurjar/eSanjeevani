8f10e214d7bfbd548179a9e5c490a533d65a234b
manishbairagi369 <bairagimanish222@gmail.com>
2024-04-29 03:22:32 +0530

    SignIN updated

--- Frontend/src/Pages/SignIn.jsx
+++ Frontend/src/Pages/SignIn.jsx
@@ -1,64 +1,62 @@
-import { useState } from "react"
-import { Link } from 'react-router-dom'
 
-const Signin = () => {
-
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  })
-
-  const handleInputChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
-
+const AdditionalDetails = () => {
   return (
-    <section className="px-5 lg:px-0">
-      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
-        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10 "> Hello! 
-          <span className="text-primaryColor px-2 ">
-            Welcome   
-          </span>
-          Back
-        </h3>
+      <div className=" bg-slate-100">
 
-        <form className="py-4 md:py-0">
-          <div className="mb-5">
-            <input type="email"
-              placeholder=" Enter your Email"
-              name="email" value={formData.email}
-              onChange={handleInputChange}
-              className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor  cursor-pointer"
-              required
-            />
-          </div>
+    <div className="flex justify-center items-center h-screen">
 
-          <div className="mb-5">
-            <input type="password"
-              placeholder="Password"
-              name="password" value={formData.password}
-              onChange={handleInputChange}
-              className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor  cursor-pointer"
-              required
-            />
-          </div>
-
-          <div className="mt-7">
-            <button type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 " >
-              Signin
-            </button>
-          </div>
-
-          <p className="mt-5 text-textColor text-center">
-            Don't have an account?
-            <Link to='/register' className="text-primaryColor font-medium ">
-              Register
-            </Link>
-          </p>
-        </form>
+  <div className="border border-solid p-16 pt-12  rounded-xl border-black shadow-2xl    ">
+    <form className="max-w-md mx-auto">
+   
+    <h2 className="font-bold  text-black mb-10 text-6xl">Log In</h2>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="email"
+          name="floating_email"
+          id="floating_email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_email"
+          className=" text-xl peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Email address
+        </label>
       </div>
-    </section>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="password"
+          name="floating_password"
+          id="floating_password"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_password"
+          className="text-xl peer-focus:font-medium absolute text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Password
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+      <p className="text-black">Don't have an account? <a href="./Signup.jsx">Register</a></p>
+    
+       
+        </div>
+        <button className="btn mt-4 w-full text-white">Log In</button>
+      </form>
+      </div>
+      </div>
+
+      
+          
+    
+    </div>
+    
   )
 }
 
-export default Signin;
+export default AdditionalDetails
